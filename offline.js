47368cfd64dc45516b1eb7f1c975b6dd279e9468
Ivan3424 <wwj3424v@163.com>
2018-07-06 15:23:52 +0800

    first

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1530861814,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/frame-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/coinboard-sheet0.png",
+		"images/moveto-sheet0.png",
+		"images/scoretrigger-sheet0.png",
+		"images/score.png",
+		"images/coin.png",
+		"images/black-sheet0.png",
+		"images/continue-sheet0.png",
+		"images/coinminus-sheet0.png",
+		"images/back-sheet0.png",
+		"images/back-sheet1.png",
+		"images/pluscoin-sheet0.png",
+		"images/plusscore-sheet0.png",
+		"images/coin2.png",
+		"images/startchar-sheet0.png",
+		"images/title-sheet0.png",
+		"images/bg_highscore-sheet0.png",
+		"images/bg_coin-sheet0.png",
+		"images/start-sheet0.png",
+		"images/start-sheet1.png"
+	]
+}
